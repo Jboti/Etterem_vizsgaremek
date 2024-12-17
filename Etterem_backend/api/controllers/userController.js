204067678a05b6d46651906ef2cefbb9566c9a94
Jboti <jboti95@gmail.com>
2024-12-17 10:13:15 +0100

    user lekérés, LOGO

--- Etterem_backend/api/controllers/userController.js
+++ Etterem_backend/api/controllers/userController.js
@@ -1,13 +1,36 @@
-const users = [
-    {
-        id:1,
-        name:"sanyi"
-    }
-]
-
 const userRepository = require('../repositories/userRepository')
 
-exports.getUser = (req,res,next) =>
+exports.getUser = async (req,res,next) =>
 {
-    res.status(200).json(users)
+    try {
+        const user = await userRepository.getUser(1)
+        res.status(200).json(user)
+    } catch (error) {
+        console.error("Error getting user:", error)
+    }
+}
+
+exports.addUser = async (req,res,next) =>
+{
+    console.log('ads')
+    try {
+        const currentDate = new Date();
+    
+        await userRepository.createUser({
+            id: null,
+            timestamp: currentDate.toISOString(),
+            created: currentDate.toISOString(),
+            userName: 'béla',
+            fullName: 'Big Béla',
+            email: 'bigbela@gmail.com',
+            password: 'asd',
+            points: 200,
+            isAdmin: true,
+            isActive: true
+        })
+        console.log("User created successfully")
+        res.status(201).send("User created successfully")
+    } catch (error) {
+        console.error("Error creating user:", error)
+    }
 }
--- Etterem_backend/api/routes/userRoutes.js
+++ Etterem_backend/api/routes/userRoutes.js
@@ -3,34 +3,10 @@ const router = express.Router()
 
 const userController = require('../controllers/userController')
 
-router.get("/", userController.getUser)
+router.get("/getUser", userController.getUser)
+router.post("/addUser", userController.addUser)
 
-//működő user beszúrás
-const userRepo = require('../repositories/userRepository')
 
-async function createInitialUser() {
-    try {
-        const currentDate = new Date();
-       
-        await userRepo.createUser({
-            id: null,
-            timestamp: currentDate.toISOString(),
-            created: currentDate.toISOString(),
-            userName: 'béla',
-            fullName: 'Big Béla',
-            email: 'bigbela@gmail.com',
-            password: 'asd',
-            points: 200,
-            isAdmin: true,
-            isActive: true
-        })
-        console.log("User created successfully")
-    } catch (error) {
-        console.error("Error creating user:", error)
-    }
-}
 
-createInitialUser()
-//eddig
 
 module.exports = router
