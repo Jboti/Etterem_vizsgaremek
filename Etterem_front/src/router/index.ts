329f3320ba4af1d777d80c3d23ae28c16879e4e4
Jboti <jboti95@gmail.com>
2024-12-20 08:37:22 +0100

    nav

--- Etterem_front/src/router/index.ts
+++ Etterem_front/src/router/index.ts
@@ -1,10 +1,8 @@
 import { createRouter, createWebHistory } from 'vue-router'
 import HomeView from '../views/HomeView.vue'
 import RendelesView from '@/views/RendelesView.vue'
-import TeamView from '@/views/TeamView.vue'
-import BlogView from '@/views/BlogView.vue'
-import ServicesView from '@/views/ServicesView.vue'
-import ContactView from '@/views/ContactView.vue'
+import FelhasznaloView from '@/views/FelhasznaloView.vue'
+import MenuView from '@/views/MenuView.vue'
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
@@ -20,24 +18,14 @@ const router = createRouter({
       component: RendelesView,
     },
     {
-      path: '/team',
-      name: 'Team',
-      component: TeamView,
+      path: '/menu',
+      name: 'Menu',
+      component: MenuView,
     },
     {
-      path: '/blog',
-      name: 'Blog',
-      component: BlogView,
-    },
-    {
-      path: '/services',
-      name: 'Services',
-      component: ServicesView,
-    },
-    {
-      path: '/contact',
-      name: 'Contact',
-      component: ContactView,
+      path: '/felhasznalo',
+      name: 'Felhasznalo',
+      component: FelhasznaloView,
     },
   ],
 })
